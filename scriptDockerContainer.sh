3c2275b454272a54aefd14ff187b9e3270d6e14b
renegc3pillar <rene.gonzalez@3pillarglobal.com>
2023-07-30 13:44:35 -0700

    Added new files

--- scriptDockerContainer.sh
+++ scriptDockerContainer.sh
@@ -16,11 +16,21 @@ fi
 #    fi
 #done
 
-echo "${MYPATH,,}/dockerfile"
+echo "${MYPATH}/Dockerfile"
 
-# create the image from the docker file
-docker build -t web-app-image -f "${MYPATH,,}/dockerfile"
-
+# create the image
+docker build -t web-app-image -f "${MYPATH}/Dockerfile" .
 
 # create the container from the image
 docker container create -i -t --name web-app-container web-app-image
+
+# mapping the container to the port 8080 
+docker run -p 8080:8080 -d -v /c/temp-keys:/root/.aspnet/DataProtection-Keys  web-app-image
+
+
+# create the MYSQL container
+docker volume prune
+docker volume create mysql-db-data
+docker run -d -p 33060:3306 --name mysql-db -e MYSQL_ROOT_PASSWORD=D@cker1234 --mount src=mysql-db-data,dst=/var/lib/mysql mysql
+docker exec -it mysql-db mysql -p
+
